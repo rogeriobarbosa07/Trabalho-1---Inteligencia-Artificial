fb6ce5f040f83bd0ab553e3419dd9798dad4dcc8
Rogério Barbosa <rogeriodosanjosbarbosa2004@gmail.com>
2025-06-24 20:04:07 -0300

    Personalizando estilo do tabuleiro

--- teste_tabuleiro.py
+++ teste_tabuleiro.py
@@ -1,16 +1,21 @@
+from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Criando a matriz 8x8 alternando 0 e 1
 tabuleiro = np.zeros((8, 8))
 tabuleiro[1::2, ::2] = 1
 tabuleiro[::2, 1::2] = 1
 
-fig = plt.figure(figsize=(4, 4), facecolor='lightskyblue', layout='constrained')
-fig.suptitle('Tabuleiro')
-ax = fig.add_subplot()
-ax.set_title('Tabuleiro de Xadrez', loc='left', fontstyle='oblique', fontsize='medium')
-ax.imshow(tabuleiro, cmap='gray', extent=[0, 8, 0, 8])
+# Personalizando o colormap do tabuleiro
+colormap_personalizado = ListedColormap(["#f0d9b5", "#a57853"])
 
+fig = plt.figure(figsize=(4, 4), facecolor='white', layout='constrained')
+ax = fig.add_subplot()
+ax.set_title('Tabuleiro de Xadrez 8x8 com 8 raínhas', fontsize='medium')
+ax.imshow(tabuleiro, cmap=colormap_personalizado, extent=[0, 8, 0, 8])
+
+# Removendo os eixos
 ax.set_xticks([])
 ax.set_yticks([])
 
