5c2cbf83737b1d0357d8b9ed88f9e7315a958a20
Rogério Barbosa <rogeriodosanjosbarbosa2004@gmail.com>
2025-06-26 20:49:35 -0300

    Mudanças (2) no código do hill climbing estocástico

--- Questão 1/stochastic_hill_climbing.py
+++ Questão 1/stochastic_hill_climbing.py
@@ -1,9 +1,10 @@
+import copy
 import random
 
 def chute_inicial():
     return [random.randint(0, 7) for _ in range(8)]
 
-def contar_ataques(posicoes):
+def conta_ataques(posicoes):
     ataques = 0
     for i in range(8):
         for j in range(i + 1, 8):
@@ -11,27 +12,20 @@ def contar_ataques(posicoes):
                 ataques += 1
     return ataques
 
-def gerar_vizinhos(posicoes):
-    vizinhos = []
-    for col in range(8):
-        for linha in range(8):
-            if linha != posicoes[col]:
-                nova_posicao = posicoes[:]
-                nova_posicao[col] = linha
-                vizinhos.append(nova_posicao)
-    return vizinhos
+def melhores_vizinhos(posicoes, n):
+    melhores = []
+    for i in range(8):
+        posicoes_copia = copy.deepcopy(posicoes)
+        posicoes_copia[n] = i
+        if conta_ataques(posicoes_copia) < conta_ataques(posicoes):
+            melhores.append(i)
+    return melhores
 
 def hill_climbing_estocastico():
-    atual = chute_inicial()
-    while True:
-        atual_ataques = contar_ataques(atual)
-        if atual_ataques == 0:
-            return atual
-        
-        vizinhos = gerar_vizinhos(atual)
-        melhores = [v for v in vizinhos if contar_ataques(v) < atual_ataques]
-
-        if not melhores:
-            return atual
-        
-        atual = random.choice(melhores)
+    posicoes = chute_inicial()
+    while conta_ataques(posicoes) > 0:
+        for i in range(8):
+            melhores = melhores_vizinhos(posicoes, i)
+            if len(melhores) > 0:
+                posicoes[i] = random.choice(melhores)
+    return posicoes
