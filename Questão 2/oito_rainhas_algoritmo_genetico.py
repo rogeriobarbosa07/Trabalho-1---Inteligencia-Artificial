01d7849867d79ed7644dd93e1ac7f7db453898e5
Rogério Barbosa <rogeriodosanjosbarbosa2004@gmail.com>
2025-07-01 09:01:27 -0300

    Adicionando pseudocódigo do Algoritmo Genético

---
+++ Questão 2/oito_rainhas_algoritmo_genetico.py
@@ -0,0 +1,17 @@
+import random
+
+'''
+PSEUDOCÓDIGO:
+
+p = inicializa_populacao()
+fp = calcula_fitness()
+
+while(critério de parada não é alcançado):
+    p' = selecao_dos_pais({p, fp})
+    p" = cruzamento(p')
+    p"' = mutacao(p")
+    fp"' = calcula_fitness(p"')
+    {p, fp} = selecao_dos_sobreviventes({p, fp}, {p"', fp"'})
+
+return melhor_solucao(p)
+'''
