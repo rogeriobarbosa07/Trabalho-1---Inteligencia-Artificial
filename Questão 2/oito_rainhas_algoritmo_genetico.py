3235d24c710159974c36e4da2c299a6eb1cf7194
Rogério Barbosa <rogeriodosanjosbarbosa2004@gmail.com>
2025-07-02 22:55:53 -0300

    Adicionando funções do SHC no AG para seleção dos pais

--- Questão 2/oito_rainhas_algoritmo_genetico.py
+++ Questão 2/oito_rainhas_algoritmo_genetico.py
@@ -1,13 +1,34 @@
-import random
+import random, copy
+
+def conta_ataques(posicoes):
+    ataques = 0
+    for i in range(8):
+        for j in range(i + 1, 8):
+            if posicoes[i] == posicoes[j] or abs(posicoes[i] - posicoes[j]) == abs(i - j):
+                ataques += 1
+    return ataques
+
+def melhores_vizinhos(posicoes, n):
+    melhores = []
+    for i in range(8):
+        posicoes_copia = copy.deepcopy(posicoes)
+        posicoes_copia[n] = i
+        if conta_ataques(posicoes_copia) < conta_ataques(posicoes):
+            melhores.append(i)
+    return melhores
 
 def inicializa_populacao():
     pop = []
     for i in range(20):
         pop_i = []
         for j in range(8):
-            pop_i.append(random.randint(0, 7))
+            pop_i.append(bin(random.randint(0, 7)))
         pop.append(pop_i)
     return pop
+
+def selecao_dos_pais(pop):
+
+    return
 
 '''
 PSEUDOCÓDIGO:
